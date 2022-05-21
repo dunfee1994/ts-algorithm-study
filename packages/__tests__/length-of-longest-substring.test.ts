e41ebb726188ef6622e321cdddd1e95591f6a758
dunfee1994 <2356648032@qq.com>
2022-05-21 22:12:09 +0800

    test: lengthOfLongestSubstring 找出字符串 str 其中不含有重复字符的 最长子串 的长度

---
+++ packages/__tests__/length-of-longest-substring.test.ts
@@ -0,0 +1,22 @@
+import lengthOfLongestSubstring from '../lib/length-of-longest-substring'
+
+describe('找出字符串 str 其中不含有重复字符的最长子串的长度', () => {
+    it('字符串 str 不包含重复字符', () => {
+        const str = 'abcdefg'
+        expect(lengthOfLongestSubstring(str)).toBe(str.length)
+    })
+
+    it('字符串 str 重复出现一个字符', () => {
+        expect(lengthOfLongestSubstring('aaaaaaaa')).toBe(1)
+    })
+
+    it('字符串 str 包含重复字符，出现的位置随意', () => {
+        expect(lengthOfLongestSubstring('abckabba')).toBe(4)
+
+        expect(lengthOfLongestSubstring('aaabbbccc')).toBe(2)
+
+        expect(lengthOfLongestSubstring('aaabbbccc')).toBe(2)
+
+        expect(lengthOfLongestSubstring('1211234aabbc')).toBe(5)
+    })
+})
