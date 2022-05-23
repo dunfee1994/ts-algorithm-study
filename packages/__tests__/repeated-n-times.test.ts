09a2efbd4793c8a051f16a9a87b8166955125790
dunfee1994 <2356648032@qq.com>
2022-05-23 15:10:29 +0800

    test: 完善用例

--- packages/__tests__/repeated-n-times.test.ts
+++ packages/__tests__/repeated-n-times.test.ts
@@ -12,12 +12,14 @@ describe('在长度 2N 的数组中找出重复 N 次的元素', () => {
     })
 
     it('数组长度为 4', () => {
-        expect(repeatedNTimes([6, 6, 2])).toBe(6)
-        expect(repeatedNTimes([6, 2, 6])).toBe(6)
-        expect(repeatedNTimes([2, 6, 6])).toBe(6)
+        expect(repeatedNTimes([6, 6, 2, 3])).toBe(6)
+        expect(repeatedNTimes([2, 6, 6, 3])).toBe(6)
+        expect(repeatedNTimes([2, 3, 6, 6])).toBe(6)
 
-        expect(repeatedNTimes([6, 6, 6])).toBe(6)
+        expect(repeatedNTimes([6, 2, 6, 3])).toBe(6)
+        expect(repeatedNTimes([2, 6, 3, 6])).toBe(6)
+        expect(repeatedNTimes([6, 2, 3, 6])).toBe(6)
 
-        expect(repeatedNTimes([1, 2, 3])).toBe(-1)
+        expect(repeatedNTimes([1, 2, 3, 4])).toBe(-1)
     })
 })
