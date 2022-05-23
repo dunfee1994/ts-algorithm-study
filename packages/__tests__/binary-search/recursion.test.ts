48f4d5371f44fa287925cbeaf43bdc44800e2531
dunfee1994 <2356648032@qq.com>
2022-05-23 17:32:48 +0800

    test: 二分法查找（递归）

---
+++ packages/__tests__/binary-search/recursion.test.ts
@@ -0,0 +1,19 @@
+import binarySearchRecursion from '../../lib/binary-search/recursion'
+
+describe('二分法查找（递归）', () => {
+    it('正常情况', () => {
+        expect(binarySearchRecursion([], 3)).toBe(-1)
+
+        expect(binarySearchRecursion([1], 5)).toBe(-1)
+
+        expect(binarySearchRecursion([1, 2, 3, 4], 3)).toBe(2)
+    })
+
+    it('非正常情况', () => {
+        expect(binarySearchRecursion()).toBe(-1)
+
+        expect(binarySearchRecursion(undefined, 3)).toBe(-1)
+        
+        expect(binarySearchRecursion([1, 2, 3, 4])).toBe(-1)
+    })
+})
