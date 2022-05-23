a0215c126810d329657bdf37455c17f89500fcf1
dunfee1994 <2356648032@qq.com>
2022-05-23 17:32:37 +0800

    test: 二分法查找（循环）

---
+++ packages/__tests__/binary-search/cycle.test.ts
@@ -0,0 +1,19 @@
+import binarySearchCycle from '../../lib/binary-search/cycle'
+
+describe('二分法查找（循环）', () => {
+    it('正常情况', () => {
+        expect(binarySearchCycle([], 3)).toBe(-1)
+
+        expect(binarySearchCycle([1], 5)).toBe(-1)
+
+        expect(binarySearchCycle([1, 2, 3, 4], 3)).toBe(2)
+    })
+
+    it('非正常情况', () => {
+        expect(binarySearchCycle()).toBe(-1)
+
+        expect(binarySearchCycle(undefined, 3)).toBe(-1)
+        
+        expect(binarySearchCycle([1, 2, 3, 4])).toBe(-1)
+    })
+})
