d362f76164d67cb2783fd08d4b98701b23e96f78
dunfee1994 <2356648032@qq.com>
2022-05-23 09:12:36 +0800

    test: 补充测试（原链表不会造成影响）

--- packages/__tests__/related-to-linked-list/reverse-linked-list.test.ts
+++ packages/__tests__/related-to-linked-list/reverse-linked-list.test.ts
@@ -5,7 +5,11 @@ describe('反转链表', () => {
     it('有多个节点的链表', () => {
         const head = createLinkedList([1, 2, 3, 4])
         const expected = createLinkedList([4, 3, 2, 1])
+
+        // 期望得到想要的结果
         expect(reverseLinkedList(head)).toEqual(expected)
+        // 期望对原链表不会造成影响
+        expect(head).toEqual(createLinkedList([1, 2, 3, 4]))
     })
 
     it('有 1 个节点的链表', () => {
