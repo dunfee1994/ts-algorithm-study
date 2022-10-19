c29a36a7b65b7d9203aeb4a1c2f4a83c08ab1bbb
dunfee1994 <2356648032@qq.com>
2022-10-19 17:33:45 +0800

    refactor: 优化及格式化调整修改

--- packages/lib/numeric-array-sort/quickly-sort.ts
+++ packages/lib/numeric-array-sort/quickly-sort.ts
@@ -3,22 +3,15 @@ import swap from './swap'
 function partition(list: number[], left: number, right: number): number {
     const pivot = list[(left + right) / 2 >>> 0]
 
-    let i = 0
-    let j = right
+    while (left <= right) {
+        while (list[left] < pivot) left++
+        
+        while (list[right] > pivot) right--
 
-    while (i <= j) {
-        while (list[i] < pivot) i++
-
-        while (list[j] > pivot) j--
-
-        if (i <= j) {
-            swap(list, i, j)
-            i++
-            j--
-        }
+        left <= right && swap(list, left++, right--)
     }
 
-    return i
+    return left
 }
 
 export default function quicklySort(list: number[], left: number = 0, right: number = list.length - 1): number[] {
