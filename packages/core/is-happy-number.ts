4136b31dce7ebe83c4123ee28187aee4b860480b
dunfee1994 <2356648032@qq.com>
2023-02-21 09:31:44 +0800

    refactor: 对 `isHappyNumber` 进行优化调整

--- packages/core/is-happy-number.ts
+++ packages/core/is-happy-number.ts
@@ -2,9 +2,9 @@
  * 判断一个正整数 num 是不是快乐数。
  * 
  * 定义为：
- * 1. 对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
- * 2. 然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
- * 3. 如果这个过程 结果为 1，那么这个数就是快乐数。
+ * 1. 对于一个正整数 num，每一次将该数替换为它每个位置上的数字的平方和。
+ * 2. 然后重复这个过程直到这个数变为 1，也可能是无限循环但始终变不到 1。
+ * 3. 如果这个过程结果为 1，那么这个数 num 就是快乐数。
  *
  * @param {number} num 正整数
  *
@@ -14,7 +14,7 @@ export default function isHappyNumber(num: number): boolean {
     const set = new Set()
 
     let sum = num
-    while (!set.has(sum)) {
+    while (sum > 2 && !set.has(sum)) {
         const str = sum.toString()
 
         set.add(sum)
@@ -23,11 +23,7 @@ export default function isHappyNumber(num: number): boolean {
         for (let i = 0, len = str.length; i < len; i++) {
             sum += Math.pow(+str[i], 2)
         }
-
-        if (sum === 1) {
-            return true
-        }
     }
 
-    return false
+    return sum === 1
 }
