36c96c68008a3c56e30d5f22f3221ccb2850f91e
dunfee1994 <2356648032@qq.com>
2023-02-21 09:31:57 +0800

    test: 对 `isHappyNumber` 进行优化调整

--- packages/__tests__/is-happy-number.test.ts
+++ packages/__tests__/is-happy-number.test.ts
@@ -1,23 +1,56 @@
 import isHappyNumber from '../core/is-happy-number'
 
 describe('isHappyNumber 判断是否是快乐数', () => {
-    it('传入 0', () => {
+    it('正整数 num 等于 0', () => {
         expect(isHappyNumber(0)).toBeFalsy()
     })
 
-    it('传入 1', () => {
+    it('正整数 num 等于 1', () => {
         expect(isHappyNumber(1)).toBeTruthy()
     })
 
-    it('传入 2', () => {
+    it('正整数 num 等于 2', () => {
         expect(isHappyNumber(2)).toBeFalsy()
     })
 
-    it('传入 7', () => {
+    it('正整数 num 等于 3', () => {
+        expect(isHappyNumber(3)).toBeFalsy()
+    })
+
+    it('正整数 num 等于 4', () => {
+        expect(isHappyNumber(4)).toBeFalsy()
+    })
+
+    it('正整数 num 等于 5', () => {
+        expect(isHappyNumber(5)).toBeFalsy()
+    })
+
+    it('正整数 num 等于 6', () => {
+        expect(isHappyNumber(6)).toBeFalsy()
+    })
+
+    it('正整数 num 等于 7', () => {
         expect(isHappyNumber(7)).toBeTruthy()
     })
 
-    it('传入 19', () => {
-        expect(isHappyNumber(7)).toBeTruthy()
+    it('正整数 num 等于 8', () => {
+        expect(isHappyNumber(8)).toBeFalsy()
+    })
+
+    it('正整数 num 等于 9', () => {
+        expect(isHappyNumber(9)).toBeFalsy()
+    })
+
+    it('正整数 num 等于 10', () => {
+        expect(isHappyNumber(10)).toBeTruthy()
+    })
+
+    it('正整数 num 等于 19', () => {
+        expect(isHappyNumber(19)).toBeTruthy()
+    })
+
+    it('正整数 num 等于 20', () => {
+        expect(isHappyNumber(20)).toBeFalsy()
     })
 })
+
