ec1925af79d55717a44819be6f0812e705f53071
dunfee1994 <2356648032@qq.com>
2023-03-29 16:35:24 +0800

    feat: 补充 `冒泡排序` 和 `插入排序` 的优化版

--- packages/core/numeric-array-sort/bubble-sort.ts
+++ packages/core/numeric-array-sort/bubble-sort.ts
@@ -14,3 +14,24 @@ export default function bubbleSort(list: number[]): number[] {
 
     return list
 }
+
+// 优化版
+export function bubbleSortOptimized(list: number[]): number[] {
+    if (new Set(list).size <= 1) return list
+
+    const len = list.length
+    for (let i = len - 1; i >= 0; i--) {
+        let flag = true
+
+        for (let j = len - 1; j >= len - i; j--) {
+            if (list[j - 1] > list[j]) {
+                swap(list, j, j - 1)
+                flag = false
+            }
+        }
+
+        if (flag) break
+    }
+
+    return list
+}
--- packages/core/numeric-array-sort/insertion-sort.ts
+++ packages/core/numeric-array-sort/insertion-sort.ts
@@ -2,7 +2,7 @@ export default function insertionSort(list: number[]): number[] {
     if (new Set(list).size <= 1) return list
 
     const endIndex = list.length - 1
-    for (let i = endIndex; i >= 0; i--) {
+    for (let i = endIndex - 1; i >= 0; i--) {
         const tmp = list[i]
 
         let j = i + 1
@@ -11,6 +11,35 @@ export default function insertionSort(list: number[]): number[] {
         }
 
         list[j - 1] = tmp
+    }
+
+    return list
+}
+
+// 优化版
+export function insertionSortOptimized(list: number[]): number[] {
+    if (new Set(list).size <= 1) return list
+
+    const endIndex = list.length - 1
+    for (let i = endIndex - 1; i >= 0; i--) {
+        const tmp = list[i]
+
+        let r = endIndex
+        for (let l = i + 1; l <= r;) {
+            const mid = l + (r - l >> 1)
+            const midVal = list[mid]
+
+            if (midVal >= tmp) {
+                r = mid - 1
+            } else {
+                l = mid + 1
+            }
+        }
+
+        for (let j = i + 1; j <= r; j++) {
+            list[j - 1] = list[j]
+        }
+        list[r] = tmp
     }
 
     return list
