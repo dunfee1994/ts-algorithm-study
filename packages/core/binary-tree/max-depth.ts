e20887e3e4158d888017532daf6e874635047d6b
dunfee1994 <2356648032@qq.com>
2023-05-04 17:51:45 +0800

    feat: 优化补充调整
    1. 补充二叉树的最大深度 - 深度优先（循环栈）
    2. 补充二叉树的最大深度 - 层序遍历
    3. 优化二叉树的最大深度 - 广度优先，避免使用 `O(n)` 时间复杂度的 `shift` 操作

--- packages/core/binary-tree/max-depth.ts
+++ packages/core/binary-tree/max-depth.ts
@@ -1,32 +1,98 @@
 import { TreeNode } from './interface'
 
-// 二叉树的最大深度 - 深度优先
-export function maxDepthRecursion(root: TreeNode | null): number {
+// 二叉树的最大深度 - 深度优先（递归）
+export function maxDepthDFSRecursion(root: TreeNode | null): number {
     if (root === null) return 0
-    return Math.max(maxDepthRecursion(root.left), maxDepthRecursion(root.right)) + 1
+    return Math.max(maxDepthDFSRecursion(root.left), maxDepthDFSRecursion(root.right)) + 1
+}
+
+// 二叉树的最大深度 - 深度优先（循环栈）
+export function minDepthDFSStack(root: TreeNode | null): number {
+    if (root === null) return 0
+
+    const depthStack: number[] = [1]
+    const nodeStack: TreeNode[] = [root]
+
+    let maxDepth = 0
+
+    while (nodeStack.length > 0) {
+        const depth = depthStack.pop()
+        const curNode = nodeStack.pop()
+
+        if (curNode.left === null && curNode.right === null) {
+            maxDepth = Math.max(maxDepth, depth)
+            continue
+        }
+
+        if (curNode.right !== null) {
+            nodeStack.push(curNode.right)
+            depthStack.push(depth + 1)
+        }
+
+        if (curNode.left !== null) {
+            nodeStack.push(curNode.left)
+            depthStack.push(depth + 1)
+        }
+    }
+
+    return maxDepth
 }
 
 // 二叉树的最大深度 - 广度优先
-export function maxDepthStack(root: TreeNode | null): number {
+export function maxDepthBFS(root: TreeNode | null): number {
     if (root === null) return 0
     
-    const stack: Array<TreeNode | null> = [root]
+    const depthStack: number[] = [1]
+    const nodeStack: TreeNode[] = [root]
 
+    let maxDepth = 0
+
+    for (let idx = 0; idx < nodeStack.length; idx++) {
+        const depth = depthStack[idx]
+        const curNode = nodeStack[idx]
+
+        if (curNode.left === null && curNode.right === null) {
+            maxDepth = Math.max(maxDepth, depth)
+            continue
+        }
+
+        if (curNode.right !== null) {
+            nodeStack.push(curNode.right)
+            depthStack.push(depth + 1)
+        }
+
+        if (curNode.left !== null) {
+            nodeStack.push(curNode.left)
+            depthStack.push(depth + 1)
+        }
+    }
+
+    return maxDepth
+}
+
+// 二叉树的最大深度 - 层序遍历
+export function maxDepthLevelOrder(root: TreeNode | null): number {
+    if (root === null) return 0
+    
     let depth = 0
-    while (stack.length > 0) {
-        for (let len = stack.length; len > 0; len--) {
-            const node = stack.shift()
+    let curLevelNodeStack: TreeNode[] = [root]
+    
+    for (; curLevelNodeStack.length > 0; depth++) {
+        const nextLevelNodeStack: TreeNode[] = []
 
-            if (node.right !== null) {
-                stack.push(node.right)
+        for (let idx = 0, len = curLevelNodeStack.length; idx < len; idx++) {
+            const curNode = curLevelNodeStack[idx]
+
+            if (curNode.left !== null) {
+                nextLevelNodeStack.push(curNode.left)
             }
 
-            if (node.left !== null) {
-                stack.push(node.left)
+            if (curNode.right !== null) {
+                nextLevelNodeStack.push(curNode.right)
             }
         }
 
-        depth++
+        curLevelNodeStack = nextLevelNodeStack
     }
 
     return depth
