fc2da724f294e9bf3495cc4f1baf599b8ccd9f45
dunfee1994 <2356648032@qq.com>
2023-07-10 17:35:19 +0800

    chore: 调整任务运行的 `git` 命令信息的打印格式

--- scripts/core/gulpfile.js
+++ scripts/core/gulpfile.js
@@ -11,7 +11,7 @@ gulp.task('git-push-to-gitee', cb => {
     const branch = argv['branch'] || 'master'
 
     console.log()
-    console.log('git push gitee' + branch)
+    console.log('git push gitee ' + branch)
     console.log()
 
     runCmd('git', ['push', 'gitee', branch], (code, result) => result ? cb(result) : cb())
@@ -21,7 +21,7 @@ gulp.task('git-push-to-github', cb => {
     const branch = argv['branch'] || 'master'
 
     console.log()
-    console.log('git push origin' + branch)
+    console.log('git push origin ' + branch)
     console.log()
 
     runCmd('git', ['push', 'origin', branch], (code, result) => result? cb(result) : cb())
