fc5eb9eaaa4208d490c84a1f406587c35bfd7fa9
dunfee1994 <2356648032@qq.com>
2023-07-14 10:09:18 +0800

    fix: 订正没有正确引入 `maxDepthDFSRecursion` 的问题

--- packages/core/binary-tree/is-balanced.ts
+++ packages/core/binary-tree/is-balanced.ts
@@ -1,12 +1,12 @@
 import { TreeNode } from './interface'
-import { maxDepthRecursion } from './max-depth'
+import { maxDepthDFSRecursion } from './max-depth'
 
 // 平衡二叉树
 export default function isBalancedRecursion(root: TreeNode | null): boolean {
     if (root === null) return true
 
-    const leftDepth = maxDepthRecursion(root.left)
-    const rightDepth = maxDepthRecursion(root.right)
+    const leftDepth = maxDepthDFSRecursion(root.left)
+    const rightDepth = maxDepthDFSRecursion(root.right)
     if (Math.abs(leftDepth - rightDepth) > 1) {
         return false
     }
