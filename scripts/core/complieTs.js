a49657f70d1c8d144ae5289b0f626f912f213071
dunfee1994 <2356648032@qq.com>
2023-07-31 17:21:05 +0800

    chore: 补充对 `packages/__tests__/**/*.test.ts` 文件的处理

--- scripts/core/complieTs.js
+++ scripts/core/complieTs.js
@@ -3,7 +3,10 @@ const gulpTs = require('gulp-typescript')
 
 const tsConfig = require('../utils/getTsConfig')()
 
-const tsFiles = ['packages/core/**/*.ts']
+const tsFiles = [
+    'packages/core/**/*.ts',
+    'packages/__tests__/**/*.test.ts'
+]
 
 module.exports = function complieTs() {
     return gulp.src(tsFiles).pipe(gulpTs(tsConfig))
