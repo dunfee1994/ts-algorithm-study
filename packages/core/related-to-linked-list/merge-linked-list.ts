b1a9cdefa72c80f890486df9fbe9fcf4bc52a8a1
dunfee1994 <2356648032@qq.com>
2023-08-14 14:47:59 +0800

    feat: 合并 `k` 个升序链表

--- packages/core/related-to-linked-list/merge-linked-list.ts
+++ packages/core/related-to-linked-list/merge-linked-list.ts
@@ -40,3 +40,22 @@ export function mergeTwoLinkedList(head1: LinkedListNode | null, head2: LinkedLi
 
     return preHead.next
 }
+
+/**
+ * 给你一个链表头节点集合 `heads`，其中所有链表均按升序排序
+ *
+ * 将它们保持原有顺序合并成一个新链表
+ *
+ * @param heads 给定升序链表头节点集合 `heads`
+ *
+ * @returns 新链表头部节点
+ */
+export function mergeKLinkedList(heads: Array<LinkedListNode | null>): LinkedListNode | null {
+    return function merge(heads: Array<LinkedListNode | null>, l: number, r: number): LinkedListNode | null {
+        if (l > r) return null
+        if (l === r) return heads[l]
+
+        const mid = l + (r - l >> 1)
+        return mergeTwoLinkedList(merge(heads, l, mid), merge(heads, mid + 1, r))
+    }(heads, 0, heads.length - 1)
+}
